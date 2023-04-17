9e2596e9cfdeb6f1b7656f41e05c2214b3c8ea0f
Ilnur <129053240+Ilnur09@users.noreply.github.com>
2023-04-17 12:22:13 +0500

    Update date.php

--- HWork/php/date.php
+++ HWork/php/date.php
@@ -4,10 +4,7 @@
 // Например, 21.09.1994 – дата рождения, 29.11.2021 – текущее значение. Разность – 9882 дня.
 echo 'Сегодня ' . date('d.m.y');
 
-$myHappyBirthday = date_create('09.07.1994');
-$dateNow = date('d.m.y');
-$dateMassivHB = explode('.', $myHappyBirthday);
-$dateMassivNow = explode('.', $dateNow);
-print_r($dateMassivNow);
-$difference = $dateMassivNow - $dateMassivHB;
-echo "Разница между моим Днем рожденья и сегоднешнием днём составялет: " . $difference . ' ';
+$myHappyBirthday = date_create_from_format('09.07.1994');
+$dateNow = date_create();
+$difference = date_diff($dateNow, $dateHappyBithday, true);
+echo $difference->format('Разница между моим ДР и сегоднешним днём');
