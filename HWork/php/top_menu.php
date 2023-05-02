1aa223eb16717092d3d78a328e0f6e58dc704de4
Ilnur Iliasov <shape907@yandex.ru>
2023-05-02 20:19:02 +0500

    Nachal delat' ne v toj vetke. Add branch hw_27_04

--- HWork/php/top_menu.php
+++ HWork/php/top_menu.php
@@ -9,9 +9,6 @@ echo '<div>
          <a class="menu_top_registrations" href="../site/registrations.php" target="_self">Регистрация</a>
     </div>
     <div>
-        <a class="menu_top_registrations" href="../site/registrations.php" target="_self">Регистрация</a>
-    </div>
-    <div>
         <a class="menu_top_registrations" href="../site/singIn.php" target="_self">Авторизация</a>
     </div>
 </div>'
